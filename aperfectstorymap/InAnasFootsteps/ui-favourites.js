13d27ba236c96a9f26b93c8e9ee2cf3ecb1b83c3
Maria Lando <mmlando@gmail.com>
2026-07-27 13:13:07 +0300

    In Ana's Footsteps: update reader story map

--- aperfectstorymap/InAnasFootsteps/ui-favourites.js
+++ aperfectstorymap/InAnasFootsteps/ui-favourites.js
@@ -49,7 +49,7 @@ function isFav(id){ return favourites.includes(id); }
 function toggleFav(){
   if(!AID) return;
   if(isFav(AID)){ favourites = favourites.filter(x => x !== AID); }
-  else { favourites.push(AID); }
+  else { favourites.push(AID); if(typeof apdTrack === 'function') apdTrack('save_place', { place_id: AID }); }
   saveFavs();
   syncFavBtn(AID);
   if(savedFilterActive) applyFilters();
@@ -408,6 +408,7 @@ function openTripInMaps(){
     if(!places.length) return;
 
     const stops = places.slice(0,8);
+    if (typeof apdTrack === 'function') apdTrack('navigate_trip', { stops: stops.length });
     const travelMode = (_lastRouteStats && _lastRouteStats.travelMode === 'driving') ? 'driving' : 'walking';
     const cityName = typeof GUIDE_CITY !== 'undefined' ? GUIDE_CITY : 'City';
 
@@ -424,6 +425,7 @@ function openTripInMaps(){
 }
 
 function shareItinerary() {
+  if (typeof apdTrack === 'function') apdTrack('share_itinerary');
   if (!favourites || favourites.length === 0) {
     _toast('Bookmark some places first 🔖');
     return;
